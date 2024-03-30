6862fc019513732a216b6ea71d9796277187910b
miladd3 <milad.d3@gmail.com>
2024-03-30 22:55:49 +0100

    some config changes and file changes

---
+++ src/components/MainLogo.vue
@@ -0,0 +1,11 @@
+<script setup>
+import logo from '@/assets/logo.svg'
+</script>
+
+<template>
+  <div>
+    <img :src="logo" alt="" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="main-nav"></div>
+</template>
+
+<style scoped></style>
